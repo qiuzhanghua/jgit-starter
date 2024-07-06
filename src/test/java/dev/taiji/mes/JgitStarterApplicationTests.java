1678e8c771a5c30dd45b88e95064af76293bd6b8
qiuzhanghua <qiuzhanghua@icloud.com>
2024-07-06 19:23:47 +0800

    learn add/commit/push/read

--- src/test/java/dev/taiji/mes/JgitStarterApplicationTests.java
+++ src/test/java/dev/taiji/mes/JgitStarterApplicationTests.java
@@ -6,7 +6,9 @@ import com.jcraft.jsch.Session;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.PushResult;
 import org.eclipse.jgit.transport.SshSessionFactory;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.eclipse.jgit.transport.ssh.jsch.JschConfigSessionFactory;
@@ -24,7 +26,6 @@ import java.io.IOException;
 class JgitStarterApplicationTests {
 
     static Dotenv dotenv;
-    static String uri = "https://gitee.com/qiuzhanghua/learn-kotlin.git";
 
     @BeforeAll
     static void contextLoads() {
@@ -43,6 +44,8 @@ class JgitStarterApplicationTests {
 
     @Test
     void testCloneAndPull() throws GitAPIException, IOException {
+        String uri = dotenv.get("GIT_URI");
+        Assert.notNull(uri, "GIT_URI is required");
         String username = dotenv.get("GIT_USERNAME");
         Assert.notNull(username, "GIT_USERNAME is required");
         String password = dotenv.get("GIT_PASSWORD");
@@ -66,11 +69,12 @@ class JgitStarterApplicationTests {
             }
         };
 
-        if (new File("repo/learn-kotlin").exists()) {
-            Git git = Git.open(new File("repo/learn-kotlin"));
+        String path = "repo/" + pathOfUri(uri);
+        if (new File(path).exists()) {
+            Git git = Git.open(new File(path));
             git.pull().setCredentialsProvider(cp).call();
             git.close();
-            System.out.println("repo/learn-kotlin pulled");
+            System.out.println(path + " pulled");
         } else {
             Git clone = Git.cloneRepository()
                     .setURI(uri)
@@ -80,10 +84,70 @@ class JgitStarterApplicationTests {
 //					}
 //				})
                     .setCredentialsProvider(cp)
-                    .setDirectory(new File("repo/learn-kotlin"))
+                    .setDirectory(new File(path))
                     .call();
             clone.close();
-            System.out.println("repo/learn-kotlin cloned");
+            System.out.println(path + " cloned");
         }
+    }
+
+
+    @Test
+    void testRead() throws IOException, GitAPIException {
+        String uri = dotenv.get("GIT_URI");
+        Assert.notNull(uri, "GIT_URI is required");
+        String path = "repo/" + pathOfUri(uri);
+        Git git = Git.open(new File(path));
+        ObjectId head = git.getRepository().resolve("HEAD");
+        git.getRepository().readOrigHead();
+        if (head == null) {
+            System.out.println("No commits");
+            return;
+        }
+        git.log().all().call().forEach(commit -> {
+            System.out.println(commit.getAuthorIdent().getName() + " " + commit.getAuthorIdent().getWhen() + " " + commit.getFullMessage());
+        });
+        git.close();
+    }
+
+    @Test
+    void testAdd() throws IOException, GitAPIException {
+        String uri = dotenv.get("GIT_URI");
+        Assert.notNull(uri, "GIT_URI is required");
+        Git git = Git.open(new File("repo/" + pathOfUri(uri)));
+        if (!new File("repo/" + pathOfUri(uri) + "/tree-01").exists()) {
+            new File("repo/" + pathOfUri(uri) + "/tree-01").mkdir();
+            git.add().addFilepattern(".").call();
+            git.commit().setMessage("Add tree-01").call();
+        }
+        String txtFile = "repo/" + pathOfUri(uri) + "/tree-01/test.txt";
+        if (!new File(txtFile).exists()) {
+            new File(txtFile).createNewFile();
+            git.add().addFilepattern(".").call();
+            git.commit().setMessage("Add test.txt").call();
+        }
+        git.close();
+    }
+
+    @Test
+    void testPush() throws IOException, GitAPIException {
+        String uri = dotenv.get("GIT_URI");
+        Assert.notNull(uri, "GIT_URI is required");
+        String username = dotenv.get("GIT_USERNAME");
+        Assert.notNull(username, "GIT_USERNAME is required");
+        String password = dotenv.get("GIT_PASSWORD");
+        Assert.notNull(password, "GIT_PASSWORD is required");
+        CredentialsProvider cp = new UsernamePasswordCredentialsProvider(username, password);
+        Git git = Git.open(new File("repo/" + pathOfUri(uri)));
+        Iterable<PushResult> pushes = git.push().setCredentialsProvider(cp).call();
+        pushes.forEach(pushResult -> {
+            System.out.println(pushResult.getMessages());
+        });
+        git.close();
+    }
+
+    static String pathOfUri(String uri) {
+        String[] split = uri.split("/");
+        return split[split.length - 1].replace(".git", "");
     }
 }
